324d5206bd0c286963f7d5c6e072163032d39941
Rafael Pufal <rafael.pufal23@gmail.com>
2021-11-15 17:37:01 -0300

    atualizando

--- server/src/middlewares/errorHandling.js
+++
@@ -1,6 +0,0 @@
-const errorHandling = (err, _req, res) => {
-  console.log(err, 'error handling');
-  return res.status(err.status).json({ message: err.message });
-};
-
-module.exports = errorHandling; 
